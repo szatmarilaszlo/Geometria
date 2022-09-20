f56552bacfde15507bf381d4b5b06f3709502f93
szatmarilaszlo <szisza162@gmail.com>
2022-09-20 18:54:05 +0200

    Add project files.

---
+++ Geometria/Kor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+	internal class Kor
+	{
+		double sugar;
+		double atmero;
+
+		public double Sugar { get => sugar;
+			set 
+			{ 
+				if(value <= 0)
+				{
+					throw new ArgumentOutOfRangeException($"{value} értékkel nem szerkeszhető kör!");
+				}
+				else 
+				{
+					sugar = value;
+				}
+			}
+		}
+		public double Atmero { get => sugar * 2; }
+
+		public double Kerulet()
+		{
+			return sugar * 2 * Math.PI;
+		}
+
+		public double Terulet()
+		{
+			return sugar * sugar * Math.PI;
+		}
+	}
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Kor kor = new Kor();
+			try
+			{
+				kor.Sugar = 2;
+				Console.WriteLine($"A kör sugara: {kor.Sugar:N2}\tkerülete: {kor.Kerulet():N2}\t területe: {kor.Terulet():N2}\tátmérője: {kor.Atmero:N2}.");
+				kor.Sugar = 7;
+				Console.WriteLine($"A kör sugara: {kor.Sugar:N2}\tkerülete: {kor.Kerulet():N2}\t területe: {kor.Terulet():N2}\tátmérője: {kor.Atmero:N2}."); kor.Sugar = -2;
+				kor.Sugar = -2;
+				Console.WriteLine($"A kör sugara: {kor.Sugar:N2}\tkerülete: {kor.Kerulet():N2}\t területe: {kor.Terulet():N2}\tátmérője: {kor.Atmero:N2}."); kor.Sugar = -2;
+				kor.Sugar = 11;
+				Console.WriteLine($"A kör sugara: {kor.Sugar:N2}\tkerülete: {kor.Kerulet():N2}\t területe: {kor.Terulet():N2}\tátmérője: {kor.Atmero:N2}.");
+			}
+			catch(ArgumentOutOfRangeException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+						
+			Console.WriteLine("Program vége!");
+			Console.ReadKey();
+		}
+	}
+}
